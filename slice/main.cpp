9a06cac6f130c633ff2fd4ac0296fbb230c74b2f
Paulo Matias <matias@ifsc.usp.br>
2012-07-17 16:12:27 +0000

    slice: implemented query-range

--- slice/main.cpp
+++ slice/main.cpp
@@ -3,10 +3,40 @@
 #include <string.h>
 
 #include "common/commoninit.h"
+#include "common/sigcfg.h"
 #include "common/windowfile.h"
 
 #define DSFMT_MEXP 19937
 #include "dsfmt/dSFMT.h"
+
+static int query_range(WindowFile &infile)
+{
+    qint64 off;
+
+    infile.nextEvent();
+    off = infile.getEventOffset();
+    puts(QString("first event:\n"
+                   "  bytes:   %1\n"
+                   "  samples: %2\n"
+                   "  seconds: %3\n")
+         .arg(off)
+         .arg(off / BytesPerSample)
+         .arg((off / BytesPerSample)/(double)SamplingRate, 0, 'f', 6)
+         .toUtf8().data());
+
+    while(infile.nextEvent());
+    off = infile.getEventOffset();
+    puts(QString("last event:\n"
+                   "  bytes:   %1\n"
+                   "  samples: %2\n"
+                   "  seconds: %3\n")
+         .arg(off)
+         .arg(off / BytesPerSample)
+         .arg((off / BytesPerSample)/(double)SamplingRate, 0, 'f', 6)
+         .toUtf8().data());
+
+    return 0;
+}
 
 static int usage(const char *progname)
 {
@@ -32,6 +62,12 @@ int main(int argc, char **argv) {
     if(!strcmp(argv[1], "query-range")) {
         if(argc != 3)
             return usage(argv[0]);
+        WindowFile *infile = new WindowFile(argv[2]);
+        if(!infile->open(QIODevice::ReadOnly)) {
+            fprintf(stderr, "can't open file '%s'.\n", argv[2]);
+            return 1;
+        }
+        query_range(*infile);
     }
     else if(!strcmp(argv[1], "range")) {
         if(argc != 6)
