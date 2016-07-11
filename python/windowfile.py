78c87cb56929edbfea2538922c9ae679877654ee
Rafael Tuma Guariento <rafael.tuma.guariento@gmail.com>
2016-07-11 17:32:51 -0300

    Alteracoes no windowfile

--- python/windowfile.py
+++ python/windowfile.py
@@ -1,5 +1,9 @@
 import struct
 import numpy as np
+import sys
+
+if sys.version_info.major == 3:
+    xrange=range
 
 def readwins(f, N=np.inf):
     n = 0
@@ -9,7 +13,7 @@ def readwins(f, N=np.inf):
         if lastlenRaw == '':
             break
         lastlen, = struct.unpack('i', lastlenRaw)
-        off, samples, channels = struct.unpack('qii', f.read(8+4*2))
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
         for ch in xrange(channels):
             chid, = struct.unpack('i', f.read(4))
             wins.append(np.frombuffer(f.read(samples*4), dtype=np.float32))
@@ -25,7 +29,7 @@ def readwinsEx(f, N=np.inf):
         if lastlenRaw == '':
             break
         lastlen, = struct.unpack('i', lastlenRaw)
-        off, samples, channels = struct.unpack('qii', f.read(8+4*2))
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
         for ch in xrange(channels):
             chid, = struct.unpack('i', f.read(4))
             yield (off, ch, np.frombuffer(f.read(samples*4), dtype=np.float32))
@@ -33,18 +37,52 @@ def readwinsEx(f, N=np.inf):
             if n >= N:
                 break
 
-def readwinsEx2(f, N=np.inf):
+
+def readwinsExAllCh(f, N=np.inf):
     n = 0
     while n < N:
         lastlenRaw = f.read(4)
         if lastlenRaw == '':
             break
         lastlen, = struct.unpack('i', lastlenRaw)
-        off, samples, channels = struct.unpack('qii', f.read(8+4*2))
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
+        sigs = {}
+        for ch in xrange(channels):
+            chid, = struct.unpack('i', f.read(4))
+            sigs.update( {chid: np.frombuffer(f.read(samples*4), dtype=np.float32)} )
+            n+=1
+            if n >= N:
+                break
+        yield (off, samples, sigs)
+
+
+def readwinsEx2(f, N=np.inf):
+    n = 0
+    while n < N:
+        n+=1
+        lastlenRaw = f.read(4)
+        if lastlenRaw == '':
+            break
+        lastlen, = struct.unpack('i', lastlenRaw)
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
         for ch in xrange(channels):
             chid, = struct.unpack('i', f.read(4))
             f.read(samples*4)
         yield off
+
+def readwinsEx2Center(f, N=np.inf):
+    n = 0
+    while n < N:
+        n+=1
+        lastlenRaw = f.read(4)
+        if lastlenRaw == '':
+            break
+        lastlen, = struct.unpack('i', lastlenRaw)
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
+        for ch in xrange(channels):
+            chid, = struct.unpack('i', f.read(4))
+            f.read(samples*4)
+        yield off, center, samples
 
 def readwinsEx3(f, N=np.inf):
     n = 0
@@ -53,7 +91,7 @@ def readwinsEx3(f, N=np.inf):
         if lastlenRaw == '':
             break
         lastlen, = struct.unpack('i', lastlenRaw)
-        off, samples, channels = struct.unpack('qii', f.read(8+4*2))
+        off, samples, channels, center = struct.unpack('qiii', f.read(8+4*3))
         signals = []
         for ch in xrange(channels):
             chid, = struct.unpack('i', f.read(4))
@@ -64,12 +102,16 @@ def readwinsEx3(f, N=np.inf):
         yield (lastlen, off, samples, channels, signals)
 
 def writewin(f, win):
-    # wins = list of ( lastlen, off, samples, channels, [(chid, sig), ...] )
-    f.write(struct.pack('i',win[0]))
-    f.write(struct.pack('qii', win[1], win[2], win[3]))
-    for ch in win[4]:
-        f.write(struct.pack('i',ch[0]))
-        f.write(ch[1].tostring())
-        if ch[1] / w[3] != w[2]:
-            print "ERROR!!"
-
+    # wins = list of ( lastlen, off, samples, channels, center [(chid, sig), ...] )
+    if sys.version_info.major == 2 or True:
+        f.write(struct.pack('i',win[0]))
+        f.write(struct.pack('qiii', win[1], win[2], win[3], win[4]))
+        for ch in win[5]:
+            f.write(struct.pack('i',ch[0]))
+            f.write(ch[1].tostring())
+    elif sys.version_info.major == 3:
+        f.write(struct.pack('i',win[0]).decode())
+        f.write(struct.pack('qiii', win[1], win[2], win[3], win[4]).decode())
+        for ch in win[5]:
+            f.write(struct.pack('i',ch[0]).decode())
+            f.write(ch[1].tostring())
